2e30f4fb3b973ebeb0154e565ba7dcdb0cf44720
root <root@localhost.localdomain>
2022-08-19 11:00:26 +0200

    fix for build

--- Canale_Andrea-Rizzolo_Lorenzo/Website/svelte.config.js
+++ Canale_Andrea-Rizzolo_Lorenzo/Website/svelte.config.js
@@ -1,15 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter(),
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+    prerender: {
+      // This can be false if you're using a fallback (i.e. SPA mode)
+      default: true
+    }
+  }
 };
-
-export default config;
